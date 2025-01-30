6537c0ba928c91531de1f12a6eb08f24724c8801
programing360 <fhlimon360@gmail.com>
2025-01-30 11:35:57 +0600

    added finishing the payoo project

--- js/home.js
+++ js/home.js
@@ -3,5 +3,20 @@ document.getElementById('btn-add-money')
         event.preventDefault()
         const pinNumber = document.getElementById('input-pin-add').value
         const amountQuantity = document.getElementById('input-amount-add').value
-        console.log(pinNumber, amountQuantity)
+        const amountInteger = parseFloat(amountQuantity)
+        console.log(pinNumber, typeof amountInteger)
+
+        if(pinNumber === '1234'){
+            const currectAmount = document.getElementById('currect-amount').innerText;
+            const currectAmountInteger = parseFloat(currectAmount)
+            console.log(typeof currectAmountInteger)
+            const totalAmount = amountInteger + currectAmountInteger;
+            console.log(totalAmount)
+            document.getElementById('currect-amount').innerText = totalAmount
+            
+        }
+        else{
+            alert('your pin number in wrong!')
+        }
+        
 });
